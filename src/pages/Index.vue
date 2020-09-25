73f13f98275cca112cc12bd416afe582c825989b
Irvin Reyes <reyes.yerp@gmail.com>
2020-09-25 18:06:56 -0500

    Display all events

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,35 +1,88 @@
 <template>
   <Layout>
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
+    <v-container>
+      <v-row justify="space-around">
+        <v-col sm="6">
+          <v-tabs v-model="tab" grow>
+            <v-tab>Item One</v-tab>
+            <v-tab>Item Two</v-tab>
+            <v-tab>Item Three</v-tab>
+          </v-tabs>
+          <v-row class="justify-space-around">
+            <v-card
+              v-for="edge in $page.events.edges"
+              :key="edge.node.id"
+              width="280"
+              class="mt-5"
+            >
+              <v-img
+                class="white--text align-end"
+                height="200px"
+                :src="`http://localhost:1337${edge.node.thumbnail}`"
+              >
+                <v-card-title>{{ edge.node.title }}</v-card-title>
+              </v-img>
 
-    <h1>Hello, world!</h1>
+              <v-card-subtitle class="pb-0">
+                {{ edge.node.date }}
+              </v-card-subtitle>
 
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur
-      excepturi labore tempore expedita, et iste tenetur suscipit explicabo!
-      Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">
-        Gridsome Docs
-      </a>
-      <a
-        href="https://github.com/gridsome/gridsome"
-        target="_blank"
-        rel="noopener"
-      >
-        GitHub
-      </a>
-    </p>
+              <v-card-actions>
+                <v-btn color="orange" text>More Info</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
   </Layout>
 </template>
+
+<page-query>
+query {
+  events: allEvent {
+    edges {
+      node {
+        id
+        title
+        description
+        price
+        date
+        duration
+        thumbnail
+        image
+      }
+    }
+  }
+}
+</page-query>
 
 <script>
 export default {
   metaInfo: {
-    title: 'Hello, world!',
+    title: 'E-vents',
+  },
+  data() {
+    return {
+      tab: 0,
+    }
+  },
+  watch: {
+    tab(val) {
+      if (this.tab === 0) {
+        this.showAllEvents()
+      } else {
+        this.showEventsByType()
+      }
+    },
+  },
+  methods: {
+    showAllEvents() {
+      console.log('all')
+    },
+    showEventsByType() {
+      console.log('type')
+    },
   },
 }
 </script>
