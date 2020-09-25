8c998c7ce9a7051d1764aec461bdb44f7e857711
Irvin Reyes <reyes.yerp@gmail.com>
2020-09-25 14:52:19 -0500

    Implement the top navigation menu

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,5 +1,20 @@
 <template>
   <v-app>
+    <v-app-bar app>
+      <v-toolbar-title>E-vents</v-toolbar-title>
+      <v-text-field
+        placeholder="Search"
+        class="ml-8"
+        style="max-width: 350px"
+        prepend-inner-icon="mdi-magnify"
+        outlined
+        rounded
+        dense
+        hide-details
+      />
+      <v-spacer />
+      <v-btn>Create Event</v-btn>
+    </v-app-bar>
     <v-main>
       <slot />
     </v-main>
