980873e64bde34bcbf45b4198aa5e1c918a46ee9
Irvin Reyes <reyes.yerp@gmail.com>
2020-09-25 18:29:25 -0500

    Add category to the query of each event

--- gridsome.server.js
+++ gridsome.server.js
@@ -36,6 +36,7 @@ module.exports = function (api) {
         duration: event.duration,
         thumbnail: event.image.formats.thumbnail.url,
         image: event.image.formats.small.url,
+        category: event.category.id,
       })
     }
   })
