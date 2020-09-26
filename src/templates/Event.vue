ef2e0a5dfd6c80b01002e21b5af387acb7f56c1c
Irvin Reyes <reyes.yerp@gmail.com>
2020-09-25 20:19:06 -0500

    Display details about the event

--- src/templates/Event.vue
+++ src/templates/Event.vue
@@ -1,6 +1,8 @@
 <template>
   <Layout>
     <h1>{{ $page.event.title }}</h1>
+    <img :src="`http://localhost:1337${$page.event.image}`" />
+    <p>{{ $page.event.description }}</p>
   </Layout>
 </template>
 
@@ -9,6 +11,7 @@ query($id: ID!) {
   event(id: $id) {
     id
     title
+    description
     duration
     price
     date
